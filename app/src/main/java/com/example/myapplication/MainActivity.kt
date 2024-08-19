efee3a04586aa68a10a1b867671bf86d5b035ab1
regina <usmanova.re@yandex.ru>
2024-08-19 23:13:36 +0500

    Немного отредактировала  код, что бы можно было передвигаться по меню из любых фрагментов

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -18,16 +18,42 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Устанавливаем ActionBar
         setSupportActionBar(findViewById(R.id.top_toolbar))
 
         val navView: BottomNavigationView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
 
+        // Устанавливаем NavController для BottomNavigationView
         navView.setupWithNavController(navController)
+
+        // Устанавливаем слушатель для обработки нажатий на элементы нижнего меню
+        navView.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.navigation_home -> {
+                    navController.navigate(R.id.navigation_home) // Переход к HomeFragment
+                    true
+                }
+                R.id.navigation_basket -> {
+                    navController.navigate(R.id.navigation_basket) // Переход к BasketFragment
+                    true
+                }
+                R.id.navigation_chat -> {
+                    navController.navigate(R.id.navigation_chat) // Переход к ChatFragment
+                    true
+                }
+                R.id.navigation_menu -> {
+                    navController.navigate(R.id.navigation_info) // Переход к InfoFragment
+                    true
+                }
+                else -> false
+            }
+        }
 
         supportActionBar?.setDisplayShowTitleEnabled(false)
     }
 
+    // Метод для обработки клика на профиль
     fun onProfileClicked(view: View) {
         // Получаем NavController из фрагмента
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
@@ -35,5 +61,4 @@ class MainActivity : AppCompatActivity() {
         // Используем NavController для перехода на фрагмент профиля
         navController.navigate(R.id.user_profile)
     }
-
 }
